3a4b3a55e3b2832034b27aae93181e8e62ae39b8
Chris Kayahara <kayahara.christopher@gmail.com>
2023-04-27 02:15:31 -0400

    Add additional animations and nav links

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -6,14 +6,76 @@ function reveal() {
     for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 50;
+      // console.log(`windowHeight: ${windowHeight}, elementTop: ${elementTop}`)
   
-      if (elementTop < windowHeight - elementVisible) {
+      if (elementTop < windowHeight) {
         reveals[i].classList.add("active");
       } else {
         reveals[i].classList.remove("active");
       }
     }
   }
-  
-  window.addEventListener("scroll", reveal);
+
+window.addEventListener("scroll", reveal);
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// Get SideNav Checkbox
+const sideNavCheckbox = document.querySelector(".side-nav__checkbox");
+
+sideNavCheckbox.addEventListener('change', function() {
+  if (this.checked) {
+    disableScroll();
+  } else {
+    enableScroll();
+  }
+});
+
+const sideNavLinks = document.querySelectorAll('.side-nav__item')
+console.log(sideNavLinks);
+
+for (let i = 0; i < sideNavLinks.length; i++) {
+  sideNavLinks[i].addEventListener("click", () => {
+    sideNavCheckbox.click();
+  })
+}
